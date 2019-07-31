466696120b571ef6d7db0fd5420d01beddbd195f
v1eira <ewersonv@gmail.com>
2019-07-30 23:30:04 -0300

    criação de meetups funcionando

---
+++ src/app/controllers/MeetupController.js
@@ -0,0 +1,34 @@
+import * as Yup from 'yup';
+import { isBefore, parseISO } from 'date-fns';
+import Meetup from '../models/Meetup';
+
+class MeetupController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      file_id: Yup.number().required(),
+      description: Yup.string().required(),
+      location: Yup.string().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    if (isBefore(parseISO(req.body.date), new Date())) {
+      return res.status(400).json({ error: 'Meetup date invalid' });
+    }
+
+    const user_id = req.userId;
+
+    const meetups = await Meetup.create({
+      ...req.body,
+      user_id,
+    });
+
+    return res.json(meetups);
+  }
+}
+
+export default new MeetupController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '0c7082be08d0861e3d0fbc780ddefee5',
+  expiresIn: '7d',
+};
